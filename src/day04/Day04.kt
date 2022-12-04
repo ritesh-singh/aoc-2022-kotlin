6284ba86e43eaef6aa7a05a8d52808fa63cf7149
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2022-12-04 19:16:02 +0530

    format

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -5,14 +5,18 @@ import readInput
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.map { it.split(",", "-").map { it.toInt() } }.count {
+        return input.map {
+            it.split(",", "-").map { it.toInt() }
+        }.count {
             val (first, second, third, fourth) = it
             (first in third..fourth && second in third..fourth) || (third in first..second && fourth in first..second)
         }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { it.split(",", "-").map { it.toInt() } }.count {
+        return input.map {
+            it.split(",", "-").map { it.toInt() }
+        }.count {
             val (first, second, third, fourth) = it
             (first in third..fourth || second in third..fourth) || (third in first..second || fourth in first..second)
         }
