c128813285e9b1c724a359c6f3f7000f7b4b2270
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2022-12-04 13:54:19 +0530

    day 4

---
+++ src/day04/Day04.kt
@@ -0,0 +1,24 @@
+package day04
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.map { it.split(",", "-").map { it.toInt() } }.count {
+            val (first, second, third, fourth) = it
+            (first in third..fourth && second in third..fourth) || (third in first..second && fourth in first..second)
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.map { it.split(",", "-").map { it.toInt() } }.count {
+            val (first, second, third, fourth) = it
+            (first in third..fourth || second in third..fourth) || (third in first..second || fourth in first..second)
+        }
+    }
+
+    val input = readInput("/day04/Day04")
+    println(part1(input))
+    println(part2(input))
+}
