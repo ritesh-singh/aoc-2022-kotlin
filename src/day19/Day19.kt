21d377068913d185d9ff2bfba62c16afc484128d
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2023-01-03 00:29:58 +0530

    day 19

--- src/day19/Day19.kt
+++ src/day19/Day19.kt
@@ -47,24 +47,61 @@ private class RobotFactory(lines: List<String>) {
         var time: Int = 24
     )
 
-    private fun largestGeodeOpened(bluePrint: BluePrint): Int {
+    private fun largestGeodeOpened(bluePrint: BluePrint, part1:Boolean): Int {
         val queue = ArrayDeque<State>()
-        queue.addLast(State())
+        queue.addLast(State().copy(time = if (part1) 24 else 32))
 
         var maxGeodes = Int.MIN_VALUE
 
-        val seen = hashSetOf<State>()
+        val visited = hashSetOf<State>()
 
+        // bfs on all possible states
         while (queue.isNotEmpty()) {
-            val currState = queue.removeFirst()
-            if (seen.contains(currState))
+            var currState = queue.removeFirst()
+            if (visited.contains(currState))
                 continue
-            seen.add(currState)
+            visited.add(currState)
             if (currState.time <= 0) {
                 maxGeodes = maxOf(maxGeodes, currState.geodes)
                 continue
             }
-            queue.addLast(
+
+            // Reduce state by removing robots required per minute
+            val maxOresRequired = listOf(bluePrint.oreRReq, bluePrint.clayRReq, bluePrint.obsidianRReq.first, bluePrint.geodeRReq.first).max() // find max ores required for a robot
+            if (currState.oreRobot >= maxOresRequired) { // max ores which can be utilized for building any type of robot per minute
+                currState = currState.copy(
+                    oreRobot = maxOresRequired
+                )
+            }
+            if (currState.clayRobot >= bluePrint.obsidianRReq.second) { // max clays which can be utilized for building any type of robot per minute
+                currState = currState.copy(
+                    clayRobot = bluePrint.obsidianRReq.second
+                )
+            }
+            if (currState.obsidianRobot >= bluePrint.geodeRReq.second) { // max obsidian which can be utilized for building any type of robot per minute
+                currState = currState.copy(
+                    obsidianRobot = bluePrint.geodeRReq.second
+                )
+            }
+
+            // Reduce state by removing resources not required per minute
+            if (currState.ore >= currState.time * maxOresRequired - currState.oreRobot * (currState.time - 1)) {
+                currState = currState.copy(
+                    ore = currState.time * maxOresRequired - currState.oreRobot * (currState.time - 1)
+                )
+            }
+            if (currState.clay >= currState.time * bluePrint.obsidianRReq.second - currState.clayRobot * (currState.time - 1)) {
+                currState = currState.copy(
+                    clay = currState.time * bluePrint.obsidianRReq.second - currState.clayRobot * (currState.time - 1)
+                )
+            }
+            if (currState.obsidian >= currState.time * bluePrint.geodeRReq.second - currState.obsidianRobot *(currState.time-1)){
+                currState = currState.copy(
+                    obsidian = currState.time * bluePrint.geodeRReq.second - currState.obsidianRobot * (currState.time - 1)
+                )
+            }
+
+            queue.addLast( // don't buy any robot, just collect resources
                 currState.copy(
                     ore = currState.ore + currState.oreRobot,
                     clay = currState.clay + currState.clayRobot,
@@ -74,7 +111,7 @@ private class RobotFactory(lines: List<String>) {
                 )
             )
 
-            if (currState.ore >= bluePrint.oreRReq) {
+            if (currState.ore >= bluePrint.oreRReq) { // buy ore robot from prev robots and collect resources
                 queue.addLast(
                     currState.copy(
                         ore = currState.ore - bluePrint.oreRReq + currState.oreRobot,
@@ -87,7 +124,7 @@ private class RobotFactory(lines: List<String>) {
                 )
             }
 
-            if (currState.ore >= bluePrint.clayRReq) {
+            if (currState.ore >= bluePrint.clayRReq) { // buy clay robot from prev robots and collect resources
                 queue.addLast(
                     currState.copy(
                         ore = currState.ore - bluePrint.clayRReq + currState.oreRobot,
@@ -101,7 +138,7 @@ private class RobotFactory(lines: List<String>) {
             }
 
             if (currState.ore >= bluePrint.obsidianRReq.first && currState.clay >= bluePrint.obsidianRReq.second) {
-                queue.addLast(
+                queue.addLast( // buy obsidian robot from prev robots and collect resources
                     currState.copy(
                         ore = currState.ore - bluePrint.obsidianRReq.first + currState.oreRobot,
                         clay = currState.clay - bluePrint.obsidianRReq.second + currState.clayRobot,
@@ -114,7 +151,7 @@ private class RobotFactory(lines: List<String>) {
             }
 
             if (currState.ore >= bluePrint.geodeRReq.first && currState.obsidian >= bluePrint.geodeRReq.second) {
-                queue.addLast(
+                queue.addLast( // buy geode robot from prev robots and collect resources
                     currState.copy(
                         ore = currState.ore  - bluePrint.geodeRReq.first + currState.oreRobot,
                         obsidian = currState.obsidian - bluePrint.geodeRReq.second + currState.obsidianRobot,
@@ -130,12 +167,16 @@ private class RobotFactory(lines: List<String>) {
         return maxGeodes
     }
 
-    fun answer() {
-        println(
-            bluePrints.map {
-                it.id * largestGeodeOpened(it)
-            }.sum()
-        )
+    fun answer(part1: Boolean):Int {
+        if (part1) {
+            return bluePrints.sumOf {
+                it.id * largestGeodeOpened(it, true)
+            }
+        }
+        return bluePrints.take(3)
+            .map {
+                largestGeodeOpened(it, false)
+            }.fold(1) { a, b -> a * b }
     }
 }
 
@@ -143,19 +184,18 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val robotFactory = RobotFactory(lines = input)
-        robotFactory.answer()
-
-        return 0
+        return robotFactory.answer(true)
     }
 
 
-    val testInput = readInput("/day19/Day19_test")
-//    println(part1(testInput))
-
-    println("-----------------------------")
+    fun part2(input: List<String>): Int {
+        val robotFactory = RobotFactory(lines = input)
+        return robotFactory.answer(false)
+    }
 
     val input = readInput("/day19/Day19")
     println(part1(input))
+    println(part2(input))
 
 }
 
